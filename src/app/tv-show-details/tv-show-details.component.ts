fb88ac5a8862e12f30c852c7fd6c4005efdf1c9e
alcfeoh <1%4ZU7H$t4$y>
2023-08-11 15:28:48 -0700

    Image slider done

--- src/app/tv-show-details/tv-show-details.component.ts
+++ src/app/tv-show-details/tv-show-details.component.ts
@@ -1,18 +1,21 @@
 import {Component, Input} from '@angular/core';
 import {TvShowDetails} from '../types';
-import {DatePipe, DecimalPipe, I18nPluralPipe} from '@angular/common';
+import {DatePipe, DecimalPipe, I18nPluralPipe, NgFor} from '@angular/common';
+import {CardComponent} from "../shared/card/card.component";
 
 @Component({
   selector: 'app-tv-show-details',
   templateUrl: './tv-show-details.component.html',
   styleUrls: ['./tv-show-details.component.css'],
   standalone: true,
-  imports: [DatePipe, DecimalPipe, I18nPluralPipe]
+  imports: [DatePipe, DecimalPipe, I18nPluralPipe, CardComponent, NgFor]
 })
 export default class TvShowDetailsComponent {
 
   @Input()
   showDetails!: TvShowDetails;
+
+  currentImage!: string;
 
   back() {
     history.back();
