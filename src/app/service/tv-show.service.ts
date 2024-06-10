d121cb89dfe309cad3e6df00139ac2d089a2922c
Hank Lingle <Hank.Lingle@eblusolutions.com>
2024-06-10 13:51:34 -0400

    fix: hard type api response and fix interface to be more inline with repsone

--- src/app/service/tv-show.service.ts
+++ src/app/service/tv-show.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ITvShow } from '../interface/tv-show';
+import { IEpisoDateResponse, ITvShow } from '../interface/tv-show';
 import { Observable, map } from 'rxjs';
 
 @Injectable({
@@ -14,19 +14,10 @@ export class TvShowService {
   getTvShows(name: string = ''): Observable<ITvShow[]> {
     const url = `https://www.episodate.com/api/search?q=${name}&page=1`
 
-    return this.http.get<any>(url).pipe(
+    return this.http.get<IEpisoDateResponse>(url).pipe(
       map(response => {
-        const tvShows: ITvShow[] = response.tv_shows.map((show: any) => ({
-          id: show.id,
-          name: show.name,
-          permalink: show.permalink,
-          startDate: new Date(show.start_date).getFullYear(),
-          endDate: new Date(show.end_date).getFullYear(),
-          country: show.country,
-          network: show.network,
-          status: show.status,
-          imageThumbnailPath: show.image_thumbnail_path
-        }));
+        const tvShows = response.tv_shows;
+        tvShows.map(show => show.start_date = new Date(show.start_date).getFullYear().toString());
         return tvShows;
       })
     );
