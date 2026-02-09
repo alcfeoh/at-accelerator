c5b44be6a19ef512d1a3a39bd0fcd8ef28a51d39
Alain Chautard <alain@interstate21.com>
2026-02-09 17:40:56 +0100

    Update favorites-view.component.ts

--- src/app/favorites-view/favorites-view.component.ts
+++ src/app/favorites-view/favorites-view.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-favorites-view',
-    standalone: true,
     templateUrl: './favorites-view.component.html',
     styleUrls: ['./favorites-view.component.css']
 })
