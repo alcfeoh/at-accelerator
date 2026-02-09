bd957ccac282a1dedc7e7cb99baeb49e4db86d45
alcfeoh <alain@interstate21.com>
2026-02-09 16:44:21 +0100

    Upgrade to v19

--- src/app/favorites-view/favorites-view.component.ts
+++ src/app/favorites-view/favorites-view.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-favorites-view',
-  templateUrl: './favorites-view.component.html',
-  styleUrls: ['./favorites-view.component.css']
+    selector: 'app-favorites-view',
+    templateUrl: './favorites-view.component.html',
+    styleUrls: ['./favorites-view.component.css'],
+    standalone: false
 })
 export class FavoritesViewComponent {
 
--- src/app/tv-show-table/tv-show-table.component.ts
+++ src/app/tv-show-table/tv-show-table.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-tv-show-table',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './tv-show-table.component.html',
-  styleUrls: ['./tv-show-table.component.css']
+    selector: 'app-tv-show-table',
+    imports: [CommonModule],
+    templateUrl: './tv-show-table.component.html',
+    styleUrls: ['./tv-show-table.component.css']
 })
 export class TvShowTableComponent {
 
